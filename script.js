d3ed879424fb443f360c6a9ce54734f076e28dad
karbseseen <134410047+karbseseen@users.noreply.github.com>
2023-05-23 18:59:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Function to display a message when a navigation link is clicked
 function showMessage() {
-  alert("You clicked a navigation link!");
+	alert("You clicked a navigation link!");
 }
 
 // Get all navigation links
@@ -10,5 +10,15 @@ var navLinks = document.querySelectorAll("nav ul li a");
 
 // Attach a click event listener to each navigation link
 navLinks.forEach(function(link) {
-  link.addEventListener("click", showMessage);
+	link.addEventListener("click", showMessage);
+});
+
+
+let tg = window.Telegram.WebApp
+tg.MainButton.setText("Send")
+tg.MainButton.enable()
+tg.MainButton.show()
+
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+	tg.sendData("some string that we need to send"); 
 });
